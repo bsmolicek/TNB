6c5cbfb626fa6b11e6bd578a5292b55fce2147cf
bsmolicek <b.smolicek@gmail.com>
2026-03-31 14:08:13 +0200

    Replace Jaeger host networking with explicit port mappings for cross-platform support

--- system-x/services/jaeger/src/main/java/software/tnb/jaeger/resource/local/JaegerContainer.java
+++ system-x/services/jaeger/src/main/java/software/tnb/jaeger/resource/local/JaegerContainer.java
@@ -1,17 +1,23 @@
 package software.tnb.jaeger.resource.local;
 
+import software.tnb.jaeger.service.configuration.JaegerConfiguration;
+
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
 import java.time.Duration;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class JaegerContainer extends GenericContainer<JaegerContainer> {
 
     public JaegerContainer(String image, Map<String, String> env) {
         super(image);
         withEnv(env);
-        withNetworkMode("host");
+        // Use explicit port mappings instead of host networking to work on macOS and Windows
+        Stream.of(JaegerConfiguration.CollectorPort.values(), JaegerConfiguration.QueryPort.values())
+            .flatMap(Stream::of)
+            .forEach(port -> addFixedExposedPort(port.portNumber(), port.portNumber()));
         waitingFor(Wait.forLogMessage(".*ListenSocket created.*", 1).withStartupTimeout(Duration.ofMinutes(2)));
     }
 }
